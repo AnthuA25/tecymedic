3607ff6f57b51d085e91237b86978a7ae061ab36
Aylin <aylin_santacruz@hotmail.com>
2025-07-09 23:45:35 -0500

    feat: :construction: The create, findAll and findCode services of the patient module were developed

--- src/patients/dto/create-patient.dto.ts
+++ src/patients/dto/create-patient.dto.ts
@@ -1,16 +1,64 @@
-import { IsNotEmpty, IsString, MinLength } from "class-validator";
+import {
+    IsNotEmpty, 
+    IsString, 
+    MinLength, 
+    MaxLength, 
+    IsEmail, 
+    // IsPhoneNumber, 
+    IsOptional,
+    IsEnum
+} from "class-validator";
 
+enum DocumentType {
+    DNI = "DNI",
+    RUC = "RUC",
+    CE = "CE",
+}
 export class CreatePatientDto {
     @IsString()
     @IsNotEmpty()
     @MinLength(3)
     name: string;
-    
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     last_name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    @MaxLength(12)
     document_number: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(3)
+    @IsEnum(DocumentType)
     document_type: string;
+
+    @MinLength(3)
+    @MaxLength(50)
+    @IsEmail()
     email: string;
+
+    @IsString()
+    @MinLength(3)
+    @MaxLength(15)
+    // @IsPhoneNumber()
+    @IsOptional()
     phone: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(50)
     username: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    @MaxLength(15)
     password: string;
 }
